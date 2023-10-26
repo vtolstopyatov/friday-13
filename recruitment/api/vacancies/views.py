085a2713fe304f853d2724e5288837fdc0bc9a34
LukoninDmitryPy <liondmitry4@gmail.com>
2023-10-26 12:51:34 +0300

    add filters to cv/vacancy

--- recruitment/api/vacancies/views.py
+++ recruitment/api/vacancies/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from vacancies.models import Cv, Vacancy
 from .serializers import (CvCreateSerializer, CvSerializer,
                           VacancySerializer,)
+from .filters import CvFilter, VacancyFilter
 
 
 class CvViewSet(viewsets.ModelViewSet):
 
     queryset = Cv.objects.all()
     serializer_class = CvSerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = CvFilter
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
@@ -20,6 +24,8 @@ class VacancyViewSet(viewsets.ModelViewSet):
 
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = VacancyFilter
 
     # def get_serializer_class(self):
     #     if self.action in ('list', 'retrieve'):
