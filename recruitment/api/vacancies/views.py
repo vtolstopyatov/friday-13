f2247097a2b4510df7ed135500503b73e2dcbdcb
Vladimir Tolstopyatov <tolstopyatovvs@gmail.com>
2023-10-28 04:20:09 +0300

    add applicants to vacancy, extend applicants

--- recruitment/api/vacancies/views.py
+++ recruitment/api/vacancies/views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 
-from vacancies.models import Cv, Vacancy
+from vacancies.models import Cv, Vacancy, Applicant
 from .serializers import (CvCreateSerializer, CvSerializer,
                           VacancySerializer,)
 from .filters import CvFilter, VacancyFilter
-
+from ..applicants.serializers import ApplicantSerializer
 
 class CvViewSet(viewsets.ModelViewSet):
 
@@ -26,6 +27,16 @@ class VacancyViewSet(viewsets.ModelViewSet):
     serializer_class = VacancySerializer
     filter_backends = (DjangoFilterBackend, )
     filterset_class = VacancyFilter
+
+    @action(detail=True, methods=['get'])
+    def applicants(self, request, pk=None):
+        pages = self.paginate_queryset(
+            Applicant.objects.filter(vacancy_responses__vacancy=pk).order_by('pk')
+        )
+        serializer = ApplicantSerializer(
+            pages, many=True, context={'request': request},
+        )
+        return self.get_paginated_response(serializer.data)
 
     # def get_serializer_class(self):
     #     if self.action in ('list', 'retrieve'):
