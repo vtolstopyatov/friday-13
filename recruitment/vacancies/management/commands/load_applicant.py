6afc9722ee8f35087f7a12fd737945c93124cf14
Vladimir Tolstopyatov <tolstopyatovvs@gmail.com>
2023-10-30 00:26:08 +0300

    fix load applicants

--- recruitment/vacancies/management/commands/load_applicant.py
+++ recruitment/vacancies/management/commands/load_applicant.py
@@ -25,33 +25,32 @@ class Command(BaseCommand):
             email = username + '@adm.adm'
             url = 'uploads/2023/10/29/photo_2023-10-04_11-36-09.jpg'
             student, _ = User.objects.update_or_create(
-                first_name=first_name,
-                last_name=last_name,
-                username=username,
                 email=email,
-                defaults={'userpic':url},
+                defaults={'userpic': url,
+                          'first_name': first_name,
+                          'last_name': last_name,
+                          'username': username},
             )
-            if not Applicant.objects.filter(student=student).exists():
-                data = {
-                    'province': random.choice(cities),
-                    'is_winner': i.get('isWinner'),
-                    'age': i.get('age'),
-                    'course': random.choice(courses),
-                    'graduation_date': date(random.randint(2018, 2023), random.randint(1, 12), random.randint(1, 27)),
-                    'work_format': random.choice(Params.SCHEDULE)[0],
-                    'schedule': random.choice(Params.SCHEDULE)[0],
-                    'contacts': email,
-                    'edu_status': random.choice(Applicant.EDU_STATUS)[0],
-                    'work_status': random.choice(Applicant.WORK_STATUS)[0],
-                    'grade': random.choice(ch.GRADE)[0],
-                    'currency': random.choice(ch.CURRENCY)[0],
-                    'salary': random.randint(1, 30)*10000,
-                    'optional_description': i.get('about'),
-                    'response_base_count': random.randint(0, 22),
-                    'test_task_count': random.randint(0, 22),
-                    'interview_count': random.randint(0, 22),
-                }
-                Applicant.objects.update_or_create(
-                    student=student,
-                    defaults=data,
-                )
+            # if not Applicant.objects.filter(student=student).exists():
+            data = {
+                'province': random.choice(cities),
+                'is_winner': i.get('isWinner'),
+                'age': i.get('age'),
+                'course': random.choice(courses),
+                'graduation_date': date(random.randint(2018, 2023), random.randint(1, 12), random.randint(1, 27)),
+                'work_format': random.choice(ch.WORK_FORMAT)[0],
+                'contacts': email,
+                'edu_status': random.choice(ch.EDU_STATUS)[0],
+                'work_status': random.choice(ch.WORK_STATUS)[0],
+                'grade': random.choice(ch.GRADE)[0],
+                'currency': random.choice(ch.CURRENCY)[0],
+                'salary': random.randint(1, 30)*10000,
+                'optional_description': i.get('about'),
+                'response_base_count': random.randint(0, 22),
+                'test_task_count': random.randint(0, 22),
+                'interview_count': random.randint(0, 22),
+            }
+            Applicant.objects.update_or_create(
+                student=student,
+                defaults=data,
+            )
