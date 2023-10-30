d06f1e2d2f4443539f030c0105d15cbceacb4e80
Vladimir Tolstopyatov <tolstopyatovvs@gmail.com>
2023-10-30 22:20:05 +0300

    fix import action

--- recruitment/api/vacancies/views.py
+++ recruitment/api/vacancies/views.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from vacancies.models import Applicant, Vacancy, VacancyResponse
 
